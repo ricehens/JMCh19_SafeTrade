e989071a57ef411172192a5653b7997c5b0751e0
Eric Shen <eric.yan.shen@gmail.com>
2022-03-11 13:04:49 -0800

    Added method specifications for Stock.java

--- Stock.java
+++ Stock.java
@@ -5,9 +5,8 @@ import java.text.DecimalFormat;
 /**
  * Represents a stock in the SafeTrade project
  */
-public class Stock
-{
-    public static DecimalFormat money = new DecimalFormat( "0.00" );
+public class Stock {
+    public static DecimalFormat money = new DecimalFormat("0.00");
 
     private String stockSymbol;
     private String companyName;
@@ -16,7 +15,47 @@ public class Stock
     private PriorityQueue<TradeOrder> buyOrders, sellOrders;
 
     // TODO complete class
+    /**
+     * Constructs a new stock with a given symbol, company name, and starting
+     * price.
+     *
+     * @param symbol the given symbol
+     * @param name the company name
+     * @param price the starting price
+     */
+    public Stock(String symbol, String name, double price) {
+        stockSymbol = symbol;
+        companyName = name;
+        loPrice = hiPrice = lastPrice = price; 
 
+        volume = 0;
+    }
+
+    /**
+     * Executes as many pending orders as possible.
+     */
+    protected void executeOrders() {
+
+    }
+
+    /**
+     * Returns a quote string for this stock.
+     *
+     * @return a quote string for this stock
+     */
+    public String getQuote() {
+
+        return "";
+    }
+
+    /**
+     * Places a trading order for this stock.
+     *
+     * @param order the order
+     */
+    public void placeOrder(TradeOrder order) {
+
+    }
     
     //
     // The following are for test purposes only
