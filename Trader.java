a8725d0482b1c69c1c4af926a44fa2d5c26a919c
jyangs <86544139+jyangs@users.noreply.github.com>
2022-03-18 23:39:21 -0700

    Update Trader.java

--- Trader.java
+++ Trader.java
@@ -13,6 +13,93 @@ public class Trader implements Comparable<Trader>
 
     // TODO complete class
 
+    public Trader(Brokerage brokerage,
+              java.lang.String name,
+              java.lang.String pswd)
+    {
+        this.brokerage = brokerage;
+        screenName = name;
+        password = pswd;
+    }
+
+    //getter methods
+    public java.lang.String getName()
+{
+    return screenName;
+}
+
+    public java.lang.String getPassword()
+    {
+        return password;
+    }
+
+    public int compareTo(Trader other)
+    {
+        int x = other.screenName.toLowerCase().hashCode();
+        int y = screenName.toLowerCase().hashCode();
+        return x-y;
+        
+    }
+
+    public boolean equals(java.lang.Object other){
+        if (other instanceof Trader){
+            Trader t= (Trader) other;
+            return (t.screenName.equals( this.screenName ));
+        }
+        else
+        throw new ClassCastException(); 
+    }
+
+    public void openWindow()
+    {
+        
+        myWindow = new TraderWindow(this);
+        if (mailbox.size()!=0){
+        for (String a: mailbox)
+        {
+        myWindow.showMessage(a);
+        mailbox.remove(a);
+        }
+    }
+    }
+
+    public boolean hasMessages()
+    {
+        if (mailbox.size()!=0)
+            return true;
+        return false;
+
+    }
+
+    public void receiveMessage(java.lang.String msg)
+    {
+        mailbox.add(msg);
+        if (myWindow!=null)
+        {
+            for (String a: mailbox)
+            {
+                myWindow.showMessage(a);
+                mailbox.remove(a);
+            }
+        }
+    }
+
+    public void getQuote(java.lang.String symbol)
+    {
+        Brokerage.getQuote(symbol);
+
+    }
+
+    public void placeOrder(TradeOrder order)
+    {
+        Brokerage.placeOrder(order);
+    }
+
+    public void quit()
+    {
+        Brokerage.logout();
+        TraderWindow.getWindows() = null;
+    }
 
     //
     // The following are for test purposes only
