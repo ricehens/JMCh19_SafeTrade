4434cf6eead2fe8511865ead8b788228a804a43e
jyangs <86544139+jyangs@users.noreply.github.com>
2022-03-20 22:34:55 -0700

    Update TradeOrder.java

--- TradeOrder.java
+++ TradeOrder.java
@@ -1,8 +1,12 @@
 import java.lang.reflect.*;
 
+
+//data carrier object to pass data about a trade order to each other
+
 /**
  * Represents a buy or sell order for trading a given number of shares of a
  * specified stock.
+ * Uses a priority queue<tradeorder>
  */
 public class TradeOrder
 {
@@ -13,7 +17,64 @@ public class TradeOrder
     private int numShares;
     private double price;
 
+   // PriorityQueue<TradeOrder> pq = new PriorityQueue<TradeOrder>();
+    //compare two tradeorder objects based on prices, ascending 
+    //or descending comparator
+
+
     // TODO complete class
+
+    public Trader getTrader()
+    {
+        return trader;
+    }
+
+    public java.lang.String getSymbol()
+    {
+        return symbol;
+    }
+
+    public boolean isBuy()
+    {
+        return buyOrder;
+    
+    }
+    public boolean isSell()
+    {
+        return !buyOrder;
+    }
+
+    public boolean isMarket()
+    {
+        return marketOrder;
+    }
+
+    public boolean isLimit()
+    {
+        return !marketOrder;
+    }
+
+    public int getShares()
+    {
+        return numShares;
+    }
+
+    //return price per share for trade order
+    public double getPrice()
+    {
+        return price;
+    }
+    
+    public void subtractShares(int shares)
+    {
+        if (shares>numShares)
+            throw new IllegalArgumentException(); 
+        else
+        {
+            numShares-= shares;
+        }
+        }
+
 
     //
     // The following are for test purposes only
