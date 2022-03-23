ca7c79f29c0169b5c3f405fd7151b7c1f4e324ce
aycode123 <61576465+aycode123@users.noreply.github.com>
2022-03-23 14:10:10 -0700

    Update PriceComparator.java

--- PriceComparator.java
+++ PriceComparator.java
@@ -5,5 +5,36 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
     // TODO complete class
+    private boolean ascending;
 
+    public PriceComparator() {
+        ascending = true;
+    }
+
+    public PriceComparator(boolean asc) {
+        ascending = asc;
+    }
+
+    public int compare(TradeOrder order1, TradeOrder order2) {
+        if (order1.isMarket() && order2.isMarket()) {
+            return 0;
+        }
+
+        else if (order1.isMarket() && order2.isLimit()) {
+            return -1;
+        }
+
+        else if (order1.isMarket() && order2.isLimit()) {
+            return -1;
+        }
+
+        else {
+            if (ascending) {
+                return (int) (order1.getPrice() - order2.getPrice()) * 100;
+            }
+            else {
+                return (int) (order2.getPrice() - order1.getPrice()) * 100;
+            }
+        }
+    }
 }
