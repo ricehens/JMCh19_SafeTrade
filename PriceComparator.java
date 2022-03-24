6185bac08c7935adfd78b3cf452dcda5fcb52ae8
aycode123 <61576465+aycode123@users.noreply.github.com>
2022-03-23 23:02:32 -0700

    added javadoc comments

--- PriceComparator.java
+++ PriceComparator.java
@@ -5,16 +5,37 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
     // TODO complete class
-    private boolean ascending;
 
+    private boolean ascending;
+    
+    /**
+     * Constructs a price comparator that compares two orders 
+     * in ascending order.
+     */
     public PriceComparator() {
         ascending = true;
     }
 
+    /**
+     * Constructs a price comparator that compares two orders 
+     * in ascending or descending order.
+     * @param asc if true, make an ascending comparator; 
+     * otherwise make a descending comparator.
+     */
     public PriceComparator(boolean asc) {
         ascending = asc;
     }
 
+    /**
+     * Compares two trade orders.
+     * @param order1 - the first order
+     * @param order2 - the second order
+     * @return 0 if both orders are market orders
+     * <li>-1 if order1 is market and order2 is limit;</li>
+     * <li>1 if order1 is limit and order2 is market;</li>
+     * <li>the difference in prices, rounded to the nearest cent, 
+     * if both order1 and order2 are limit orders.</li>
+     */
     public int compare(TradeOrder order1, TradeOrder order2) {
         if (order1.isMarket() && order2.isMarket()) {
             return 0;
@@ -24,16 +45,16 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
             return -1;
         }
 
-        else if (order1.isMarket() && order2.isLimit()) {
+        else if (order1.isLimit() && order2.isMarket()) {
             return -1;
         }
 
         else {
             if (ascending) {
-                return (int) (order1.getPrice() - order2.getPrice()) * 100;
+                return (int) ((order1.getPrice() - order2.getPrice()) * 100);
             }
             else {
-                return (int) (order2.getPrice() - order1.getPrice()) * 100;
+                return (int) ((order2.getPrice() - order1.getPrice()) * 100);
             }
         }
     }
